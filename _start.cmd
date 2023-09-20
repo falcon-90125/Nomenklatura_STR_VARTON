514c990134eeabcc8790f139c8b9c3eaef588a67
falcon-90125 <sokolov_aa@ngs.ru>
2023-09-20 18:29:13 +0700

    update _start.cmd

---
+++ _start.cmd
@@ -0,0 +1,7 @@
+# Сборка образа
+# docker build . -t str_varton_imade:latest
+
+# Сборка контейнера
+# docker run --name nom_var -v C:/Users/sokolov/MyData/Projects/nom_varton_docker_run_exchange:/app/exchange -d str_varton_imade:latest
+
+docker start nom_var
