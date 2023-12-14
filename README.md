d119c6f82b3d76df4a6793cc6cbc019ffb116296
falcon-90125 <sokolov_aa@ngs.ru>
2023-12-14 09:18:03 +0700

    upd 2023_12_14

--- README.md
+++
@@ -1,4 +0,0 @@
-# Преобразование pdf-файла номенклатуры светотехнического расчёта в excel-файл с ценами:
-## парсинг exchange/nomenkl.pdf, преобразование в файл спецификации проекта с ценами из varton_price.xlsx
-
-![review.jpg](https://github.com/falcon-90125/Nomenklatura_STR_VARTON/blob/main/review.jpg)
--- _start.cmd
+++ _start.cmd
@@ -2,6 +2,6 @@
 # docker build . -t str_varton_imade:latest
 
 # Сборка контейнера
-# docker run --name nom_var -v C:/Users/sokolov/MyData/Projects/nom_varton_docker_run_exchange:/app/exchange -d str_varton_imade:latest
+# docker run --name nom_var -v C:/Users/sokolov/MyData/Projects/Nomenklatura_STR_VARTON/exchange:/app/exchange -d str_varton_imade:latest
 
 docker start nom_var
