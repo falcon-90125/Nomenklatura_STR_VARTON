a2d0863025ec494f03924c31590695cac58578dd
falcon-90125 <sokolov_aa@ngs.ru>
2024-04-10 15:56:41 +0700

    upd path exchange

---
+++ exchange/_start.cmd
@@ -0,0 +1,7 @@
+# Сборка образа
+# docker build . -t str_varton_image:latest
+
+# Сборка контейнера
+# docker create --name nom_var -v C:/Users/sokolov/Yandex_Disk/MyData/Projects/Nomenklatura_STR_VARTON/exchange:/app/exchange -d str_varton_image:latest
+
+docker start nom_var
--- start.cmd
+++
@@ -1,7 +0,0 @@
-# Сборка образа
-# docker build . -t str_varton_imade:latest
-
-# Сборка контейнера
-# docker run --name nom_var -v C:/Users/sokolov/MyData/Projects/Nomenklatura_STR_VARTON/exchange:/app/exchange -d str_varton_imade:latest
-
-docker start nom_var
