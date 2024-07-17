3362ea3e05a5a087385500303f157ff7077e53b9
falcon-90125 <sokolov_aa@ngs.ru>
2024-07-17 10:13:08 +0700

    add bracket 2 pieces & exit

--- exchange/_start.cmd
+++ exchange/_start.cmd
@@ -2,6 +2,6 @@
 # docker build . -t str_varton_image:latest
 
 # Сборка контейнера
-# docker create --name nom_var -v C:/Users/sokolov/Yandex_Disk/MyData/Projects/Nomenklatura_STR_VARTON/exchange:/app/exchange -d str_varton_image:latest
+# docker create --name nom_var -v C:/Users/sokolov/Yandex_Disk/MyData/Projects/Nomenklatura_STR_VARTON/exchange:/app/exchange str_varton_image:latest
 
 docker start nom_var
